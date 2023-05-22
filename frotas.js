11163ee782caba2363e21b5d86c1b9560a30aca1
Luis Felipe <86193136+luis-felipee@users.noreply.github.com>
2023-05-22 12:16:11 -0300

    Versão 3.2
    Adicionei o FOOTER

--- frotas.js
+++ frotas.js
@@ -2,10 +2,10 @@ const headerElement = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     
-    if (window.scrollY > 100 ){
+    if (window.scrollY > 100 && window.scrollY <= 800){
         headerElement.classList.add('header-scrolled');
     }
-    else if(window.scrollY <=100){
+    else if(window.scrollY <=100 || window.scrollY > 800){
         headerElement.classList.remove('header-scrolled')
     }
 });
