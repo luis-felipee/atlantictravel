199dfa82947c8e67a0b389d612bb720d1b37a817
Luis Felipe <86193136+luis-felipee@users.noreply.github.com>
2023-06-16 11:10:14 -0300

    Add files via upload

--- frotas.js
+++ frotas.js
@@ -2,10 +2,10 @@ const headerElement = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     
-    if (window.scrollY > 100 && window.scrollY <= 300){
+    if (window.scrollY > 100 && window.scrollY <= 500){
         headerElement.classList.remove('header-scrolled');
     }
-    else if(window.scrollY <=100 || window.scrollY > 330 ){
+    else if(window.scrollY <=100 || window.scrollY > 500 ){
         headerElement.classList.add('header-scrolled')
     }
 });
